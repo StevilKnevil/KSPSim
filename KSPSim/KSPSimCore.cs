f5e9f256d00a180c33c21331e7dd05aa6fa71991
Stevil Knevil <stevilknevil@hotmail.co.uk>
2015-09-10 21:17:06 +0100

    Initial Commit

---
+++ KSPSim/KSPSimCore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KSPSim
+{
+  [KSPAddon(KSPAddon.Startup.EditorAny, false)]
+  public class KSPSimCore : MonoBehaviour
+  {
+    public void toggle()
+    {
+      print(">>> here <<<");
+    }
+
+    ApplicationLauncherButton alButton;
+
+    public void Update()
+    {
+      if (ApplicationLauncher.Ready && alButton == null)
+      {
+        Texture2D buttonTexture = null; //GameDatabase.Instance.GetTexture("KSPSim/Icons/KSPSim", false);
+        alButton = ApplicationLauncher.Instance.AddModApplication(
+          toggle, toggle,
+          null, null,
+          null, null,
+          ApplicationLauncher.AppScenes.ALWAYS,
+          buttonTexture);
+      }
+
+    }
+
+  }
+}
